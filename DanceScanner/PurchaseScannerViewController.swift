f4d47cd40d23217430aac8409a448b0a91e128c7
Jimmy Rodriguez <jrodriguez8064@jh-mba158-12.d214.org>
2017-12-05 08:53:45 -0600

    1st block tutorial code working  - Jim

--- DanceScanner/PurchaseScannerViewController.swift
+++ DanceScanner/PurchaseScannerViewController.swift
@@ -30,26 +30,10 @@ class PurchaseScannerViewController: UIViewController, AVCaptureMetadataOutputOb
         if (session.canAddInput(videoInput!)) {
             session.addInput(videoInput!)
         } else {
-            // scanningNotPossible()
+            scanningNotPossible()
         }
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     func scanningNotPossible() {
         let alert = UIAlertController(title: "This device can't scan.", message: "How did you mess this up? It was only supposed to be sent to camera-equipped iPads!", preferredStyle: .alert)
