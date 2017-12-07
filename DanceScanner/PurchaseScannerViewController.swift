cb77c63e46c6ad198f59f6568a7bedd1ddf55e39
Akhil Nair <anair8152@jh-mba158-11.d214.org>
2017-12-07 08:50:20 -0600

    added barcodeDetected function: does not detect school ID

--- DanceScanner/PurchaseScannerViewController.swift
+++ DanceScanner/PurchaseScannerViewController.swift
@@ -55,6 +55,37 @@ class PurchaseScannerViewController: UIViewController, AVCaptureMetadataOutputOb
         session.startRunning()
         
     }
+    func barcodeDetected(code: String){
+        // Let the user know we've found something.
+        print(code)
+        let alert = UIAlertController(title: "Found a Barcode!", message: code, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Search", style: UIAlertActionStyle.destructive, handler: { action in
+            
+            // Remove the spaces.
+            let trimmedCode = code.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+            
+            // EAN or UPC?
+            // Check for added "0" at beginning of code.
+            
+            let trimmedCodeString = "\(trimmedCode)"
+            var trimmedCodeNoZero: String
+            
+            if trimmedCodeString.hasPrefix("0") && trimmedCodeString.characters.count > 1 {
+                trimmedCodeNoZero = String(trimmedCodeString.characters.dropFirst())
+                
+                // Send the doctored UPC to DataService.searchAPI()
+//                DataService.searchAPI(trimmedCodeNoZero)
+            } else {
+//
+//                 Send the doctored EAN to DataService.searchAPI()
+//                DataService.searchAPI(trimmedCodeString)
+            }
+            
+                self.navigationController?.popViewController(animated: true)
+        }))
+        
+            self.present(alert, animated: true, completion: nil)
+    }
     
     func scanningNotPossible() {
         let alert = UIAlertController(title: "This device can't scan.", message: "How did you mess this up? It was only supposed to be sent to camera-equipped iPads!", preferredStyle: .alert)
@@ -64,12 +95,14 @@ class PurchaseScannerViewController: UIViewController, AVCaptureMetadataOutputOb
     }
     
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        print("Hello")
         if let barcodeData = metadataObjects.first {
             let barcodeReadable = barcodeData as? AVMetadataMachineReadableCodeObject
+           
             if let readableCode = barcodeReadable{
-                print("x")
+            //    print("x")
 //                delete above /\ and uncomment below \/
-//                barcodeDetected(readableCode.stringValue)
+                barcodeDetected(code: readableCode.stringValue!)
             }
             //make something other than vibrating
             
