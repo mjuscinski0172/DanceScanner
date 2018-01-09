d8693a354e411d18302c7c816dedef31deb3041a
Akhil Nair <anair8152@jh-mba158-11.d214.org>
2018-01-09 08:38:23 -0600

    connected  to cocoa touch

---
+++ DanceScanner/checkViewController.swift
@@ -0,0 +1,35 @@
+//
+//  checkViewController.swift
+//  DanceScanner
+//
+//  Created by Akhil Nair on 1/9/18.
+//  Copyright © 2018 Michal Juscinski. All rights reserved.
+//
+
+import UIKit
+
+class checkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
