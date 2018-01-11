e5f813eb7102c968d272fa0fe470b34ff3109116
Jimmy Rodriguez <jrodriguez8064@jh-mba-jacks.local>
2018-01-11 08:53:46 -0600

    added list view controller

--- DanceScanner/ListViewController.swift
+++ DanceScanner/ListViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import CloudKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let database = CKContainer.default().publicCloudDatabase
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        return cell
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 }
