1995dae12da6a2ecf38ae0e4e81d6065ea669bef
Jimmy Rodriguez <jrodriguez8064@jh-mba158-12.d214.org>
2018-01-25 08:47:41 -0600

    idk what i changed tbh

--- DanceScanner/addGuestViewController.swift
+++ DanceScanner/addGuestViewController.swift
@@ -19,12 +19,6 @@ class addGuestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func whenConfirmButtonPressed(_ sender: UIButton) {
@@ -41,17 +35,4 @@ class addGuestViewController: UIViewController {
             var guestParentNumber = parentPhoneNumberTextField.text
         }
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
