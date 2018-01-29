a7ef02575d87511717925c85db4cc62606944779
Akhil Nair <anair8152@jh-mba158-11.d214.org>
2018-01-29 08:20:50 -0600

    did all the guest stuff i think

--- DanceScanner/addGuestViewController.swift
+++ DanceScanner/addGuestViewController.swift
@@ -15,20 +15,17 @@ class addGuestViewController: UIViewController {
     @IBOutlet weak var guestSchoolTextField: UITextField!
     @IBOutlet weak var parentPhoneNumberTextField: UITextField!
     var database = CKContainer.default().publicCloudDatabase
-
+    var selectedStudentArray: NSArray!
+    var altId: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func whenConfirmButtonPressed(_ sender: UIButton) {
-
+        
         
         if guestNameTextField.text! == "" || guestSchoolTextField.text! == "" || parentPhoneNumberTextField.text! == ""{
             let alert = UIAlertController(title: "Error", message: "It appears that you have missed some information about this student. Please look back and type in ALL the information", preferredStyle: .alert)
@@ -39,19 +36,41 @@ class addGuestViewController: UIViewController {
             var guestName = guestNameTextField.text
             var guestSchool = guestSchoolTextField.text
             var guestParentNumber = parentPhoneNumberTextField.text
+            let studentDictionary = self.selectedStudentArray.firstObject as! NSDictionary
+            let firstName = studentDictionary.object(forKey: "First") as! NSString
+            let lastName = studentDictionary.object(forKey: "Last") as! NSString
+            let ID = studentDictionary.object(forKey: "ID") as! NSInteger
+            let place = CKRecord(recordType: "Students")
+            place.setObject(firstName as CKRecordValue, forKey: "firstName")
+            place.setObject(lastName as CKRecordValue, forKey: "lastName")
+            place.setObject(String(ID) as CKRecordValue, forKey: "idNumber")
+            place.setObject(String(altId) as CKRecordValue, forKey: "altIDNumber")
+            place.setObject("Purchased" as CKRecordValue, forKey: "checkedInOrOut")
+            place.setObject("" as CKRecordValue, forKey: "checkInTime")
+            place.setObject("" as CKRecordValue, forKey: "checkOutTime")
+            place.setObject(guestName as! CKRecordValue, forKey: "guestName")
+            place.setObject(guestSchool as! CKRecordValue, forKey: "guestSchool")
+            place.setObject(guestParentNumber as! CKRecordValue, forKey: "guestParentPhone")
+            
+            
+            self.database.save(place) { (record, error) in
+                if error != nil {
+                    let alert = UIAlertController(title: "Error", message: error.debugDescription, preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alert.addAction(okAction)
+                    self.present(alert, animated: true, completion: nil)
+                    
+                }
+                print(1)
+                
+                print(3)
+            }
+            print(4)
+            
+            
         }
+        self.dismiss(animated: true, completion: nil)
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     
 }
