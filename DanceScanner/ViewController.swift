7ce93c241953971a665a49aa6d86d36e1f624508
Michal Juscinski <mjuscinski0172@jh-mba158-16.d214.org>
2018-02-01 08:49:34 -0600

    added alert

--- DanceScanner/ViewController.swift
+++ DanceScanner/ViewController.swift
@@ -23,7 +23,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func deleteAllButton(_ sender: UIButton) {
-        
+        let passwordAlert = UIAlertController(title: "Delete all students?", message: "If you would like to delete all students from the database, please insert your password", preferredStyle: .alert)
+        passwordAlert.addTextField { (textField) in
+            textField.placeholder = "Password"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let confirmAction = UIAlertAction(title: "Confirm", style: .destructive) { (action) in
+            let passwordTextField = passwordAlert.textFields![0]
+            if passwordTextField.text == resetAllPassword {
+                
+            }
+            else {
+                
+            }
+        }
+        passwordAlert.addAction(cancelAction)
+        passwordAlert.addAction(confirmAction)
+        present(passwordAlert, animated: true, completion: nil)
     }
     
 }
