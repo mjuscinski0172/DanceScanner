7fa03890872ccb566be57bdceb684b080e5c1879
Michal Juscinski <mjuscinski0172@jh-mba158-16.local>
2018-02-14 08:37:23 -0600

    made studentArray for deletion

--- DanceScanner/ViewController.swift
+++ DanceScanner/ViewController.swift
@@ -11,10 +11,13 @@ import CloudKit
 
 class ViewController: UIViewController {
     var resetAllPassword = "57bw32Gc"
+    var studentArray = [Student]()
+    var database = CKContainer.default().publicCloudDatabase
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var database = CKContainer.default().publicCloudDatabase
+//        var database = CKContainer.default().publicCloudDatabase
         self.navigationController?.navigationBar.barStyle = UIBarStyle.blackTranslucent
     }
 
@@ -33,7 +36,9 @@ class ViewController: UIViewController {
             let passwordTextField = passwordAlert.textFields![0]
             if passwordTextField.text == self.resetAllPassword {
                 let areYouPositive = UIAlertController(title: "Are you sure?", message: "You cannot go back from here.", preferredStyle: .alert)
-                let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                let OKAction = UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                    self.createStudentArray()
+                })
                 let cancelAction2 = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
 
                 areYouPositive.addAction(OKAction)
@@ -54,6 +59,33 @@ class ViewController: UIViewController {
         passwordAlert.addAction(confirmAction)
         present(passwordAlert, animated: true, completion: nil)
     }
+    
+    func createStudentArray() {
+        studentArray.removeAll()
+        
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Students", predicate: predicate)
+        database.perform(query, inZoneWith: nil) { (records, error) in
+            for student in records! {
+                let firstName = student.object(forKey: "firstName") as! String
+                let lastName = student.object(forKey: "lastName") as! String
+                let altIDNumber = student.object(forKey: "altIDNumber") as! String
+                let idNumber = student.object(forKey: "idNumber") as! String
+                let checkedInOrOut = student.object(forKey: "checkedInOrOut") as! String
+                let checkInTime = student.object(forKey: "checkInTime") as! String
+                let checkOutTime = student.object(forKey: "checkOutTime") as! String
+                let guestName = student.object(forKey: "guestName") as! String
+                let guestSchool = student.object(forKey: "guestSchool") as! String
+                let guestParentPhone = student.object(forKey: "guestParentPhone") as! String
+                let newStudent = Student(firstName: firstName, lastName: lastName, altIDNumber: altIDNumber, idNumber: idNumber, checkedInOrOut: checkedInOrOut, checkInTime: checkInTime, checkOutTime: checkOutTime, guestName: guestName, guestSchool: guestSchool, guestParentPhone: guestParentPhone)
+                self.studentArray.append(newStudent)
+            }
+            DispatchQueue.main.async {
+                print("Nothing")
+            }
+        }
+    }
+    
     
 }
 
